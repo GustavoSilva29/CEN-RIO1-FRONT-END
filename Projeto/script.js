4c1f1e056184fb4d87ed8b42c61cccb05645fde5
Gustavo Aparecido da Silva <68440761+GustavoSilva29@users.noreply.github.com>
2023-03-22 18:41:32 -0300

    Update script.js
    Identando o codigo.

--- Projeto/script.js
+++ Projeto/script.js
@@ -33,16 +33,16 @@ btnComments.addEventListener('click', () => {                	 // Adiciona uma f
         commentsList.appendChild(li);
       });
     })
-    .catch(error => {				// Se a solicitação HTTP GET falhar, execute a seguinte função com o erro como parâmetro
+    .catch(error => {				    // Se a solicitação HTTP GET falhar, execute a seguinte função com o erro como parâmetro
       console.log(error);                           // Exiba o erro no console do navegador
     });
 });
 
 // Solicitação da API JSONPlaceholder para obter a lista de usuários
-axios.get('https://jsonplaceholder.typicode.com/users') //O método axios.get é usado para enviar a solicitação HTTP GET.
-  .then(response => {							// Quando a solicitação HTTP GET for bem-sucedida, execute a seguinte função
-    const users = response.data;					// Extraia os dados da resposta da solicitação HTTP e armazene-os em uma variável chamada "posts"
-    const usersList = document.querySelector('#users-list'); // seleciona o elemento HTML com o id "users-list" e armazena-o na variável usersList
+axios.get('https://jsonplaceholder.typicode.com/users')         //O método axios.get é usado para enviar a solicitação HTTP GET.
+  .then(response => {						// Quando a solicitação HTTP GET for bem-sucedida, execute a seguinte função
+    const users = response.data;				// Extraia os dados da resposta da solicitação HTTP e armazene-os em uma variável chamada "posts"
+    const usersList = document.querySelector('#users-list');    // seleciona o elemento HTML com o id "users-list" e armazena-o na variável usersList
     users.forEach(user => {					// itera sobre a lista de usuários e cria um elemento de lista para cada um
       const li = document.createElement('li');			// cria um novo elemento de lista HTML
       li.textContent = user.name;				// define o texto do elemento de lista como o nome do usuário atual
@@ -63,7 +63,7 @@ const userId = document.querySelector('#user-id').value; 	// seleciona o element
   
   // faz uma solicitação GET para a API do JSONPlaceholder para obter os detalhes do usuário correspondente ao ID inserido pelo usuário
   axios.get(`https://jsonplaceholder.typicode.com/users/${userId}`)
-    .then(response => {							 // armazena os dados do usuário na variável user
+    .then(response => {							// armazena os dados do usuário na variável user
       const user = response.data;					// cria um novo elemento HTML do tipo parágrafo e define seu conteúdo como o nome e o e-mail do usuário
       const p = document.createElement('p');
       p.innerHTML = `Nome: ${user.name}<br>Email: ${user.email}`;	// adiciona o elemento de parágrafo criado como filho do elemento HTML com o id "user-details"
